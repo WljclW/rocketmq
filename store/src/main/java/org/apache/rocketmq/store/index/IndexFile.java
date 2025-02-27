a2683bc27f75ac52cbb1d58ca168ea3a46f4e9dd
luck-jay <820173359@qq.com>
2025-02-27 20:42:37 +0800

    消息存储[03之indexFile]

--- store/src/main/java/org/apache/rocketmq/store/index/IndexFile.java
+++ store/src/main/java/org/apache/rocketmq/store/index/IndexFile.java
@@ -117,23 +117,38 @@ public class IndexFile {
     }
 
     /**
+     * 【总述】根据key、消息在CommitLog的偏移量等，创建index条目放入到Index文件
+     * 【方法的逻辑】
+     *      先是根据key计算出哈希槽的索引(取余法)；记录拿到哈希槽的值(如果值无效的话就记录为0)；记录时间差值；计算index条目应该放值的偏移量；
+     *      然后依次将index条目的各个部分放进去(step2记录的哈希槽的值放在最后4字节)
+     * 【头插法】step1:由于每一次是先拿出对应哈希槽的值(可以叫做 旧值)，step2:然后将这个"旧值"写入当前的index条目的末尾4字节中，
+     *      step3:最后更将哈希槽中的旧值更新为当前index条目的索引
+     *      ————从上述逻辑可以看出来，最后插入的index条目是
+     * 【其他想法】
      * key：消息索引；phyOffset：消息物理偏移量；storeTimestamp：消息存储时间戳
-     * 这个方法有一个严密的逻辑：如果哈希槽中存储的值为0或大于当前Index文件最大条目数或小于-1，表示该哈希
-     * 槽当前并没有与之对应的Index条目。
+     * 这个方法有一个严密的逻辑：
+     *      如果哈希槽中存储的值为0或大于当前Index文件最大条目数或小于-1，表示该哈希槽当前并没有与之对应的Index条目。(这样做相当于把哈希
+     *          冲突的问题就解决了)
+     *      如果存储的值大于0，表示该哈希槽当前有与之对应的Index条目，此时需要去获取该Index条目的物理地址，然后通过物理地址获取到该Index
+     *          条目，(然后每一个index条目的最后4字节会存放哈希冲突链中前一个index条目)
      * */
-    //将 消息索引键 与 消息偏移量 的映射关系写入index文件的实现
+    //将 消息索引键 与 消息偏移量 的映射关系写入index文件的实现。。重点关注一下方法中的绿色注释
     public boolean putKey(final String key, final long phyOffset, final long storeTimestamp) {
         if (this.indexHeader.getIndexCount() < this.indexNum) {
             int keyHash = indexKeyHashMethod(key); //计算key的哈希码
             int slotPos = keyHash % this.hashSlotNum; //计算key对应的哈希槽的下标
-            //计算key对应的哈希槽的物理地址(40字节的头部+下标*4字节index文件每个条目的长度)
+            //计算key对应的哈希槽 的物理地址(40字节的头部+下标*4字节index文件每个条目的长度)
             int absSlotPos = IndexHeader.INDEX_HEADER_SIZE + slotPos * hashSlotSize;
 
             try {
-                //这里会去拿参数key对应的哈希槽(每一个都是4字节)现在的值
+                /**
+                 * 这里会拿到哈希槽对应的值
+                 * */
+                //这里会去拿参数key对应的哈希槽(每一个都是4字节)现在的值。。
                 int slotValue = this.mappedByteBuffer.getInt(absSlotPos); //拿出存当前消息前，key对应哈希槽的值————这个值就是在最后的索引
                 if (slotValue <= invalidIndex || slotValue > this.indexHeader.getIndexCount()) {
-                    slotValue = invalidIndex;
+                    //进入到if条件说明：当前哈希槽是没有东西的————也就是说目前还没有key映射到这个哈希槽。。
+                    slotValue = invalidIndex; //将哈希槽的值置为0
                 }
 
                 //计算带存储消息 与 第一条消息时间戳 的差值，并换算为秒
@@ -149,8 +164,8 @@ public class IndexFile {
                 }
                 /*计算新添加条目的起始物理偏移量absIndexPos————
                        40字节的头部(固定40字节) +
-                       哈希槽数量*每一个哈希槽所占字节数(固定值4字节) +
-                       当前条目数量*每个条目所占的字节数(固定值20字节)
+                       哈希槽数量(通过this.hashSlotNum拿)*每一个哈希槽所占字节数(固定值4字节) +
+                       当前文件中index条目数量(通过this.indexHeader.getIndexCount()拿)*每个条目所占的字节数(固定值20字节)
                  */
                 int absIndexPos =
                     IndexHeader.INDEX_HEADER_SIZE + this.hashSlotNum * hashSlotSize
@@ -159,6 +174,10 @@ public class IndexFile {
                 this.mappedByteBuffer.putInt(absIndexPos, keyHash);
                 this.mappedByteBuffer.putLong(absIndexPos + 4, phyOffset);
                 this.mappedByteBuffer.putInt(absIndexPos + 4 + 8, (int) timeDiff);
+                /**
+                 * 这里会把上面拿到哈希槽的值赋值给index条目的最后4字节。(这样当某一个哈希槽有很多key被映射到的时候，所有的index条目就会
+                 *      形成一个链条，每一个index条目的最后4字节 就是 这个链中前一个index条目对应的索引)
+                 * */
                 this.mappedByteBuffer.putInt(absIndexPos + 4 + 8 + 4, slotValue); //index条目的最后4字节存储该哈希码上一个条目的index下标
                 //哈希槽中存储的是该哈希码对应的最新index条目的下标
                 this.mappedByteBuffer.putInt(absSlotPos, this.indexHeader.getIndexCount());
@@ -215,23 +234,33 @@ public class IndexFile {
         return result;
     }
 
+    /**
+     * 参数解释：
+     *      phyOffsets:要查找的消息的物理偏移量
+     *      key:要查找的消息的key
+     *      maxNum:最多查到多少条消息
+     *      begin:查询的起始时间戳
+     *      end:查询的结束时间戳
+     * 【总述】用于根据指定的key(参数key) 和 时间戳范围(参数begin~end决定) 查找满足条件的至多maxnNum条消息，并将这些消息的物理偏移
+     *      量放入参数phyOffsets中。
+     * */
     public void selectPhyOffset(final List<Long> phyOffsets, final String key, final int maxNum,
                                 final long begin, final long end) {
         if (this.mappedFile.hold()) {
-            int keyHash = indexKeyHashMethod(key);
-            int slotPos = keyHash % this.hashSlotNum;
-            int absSlotPos = IndexHeader.INDEX_HEADER_SIZE + slotPos * hashSlotSize;
+            int keyHash = indexKeyHashMethod(key);  //计算出哈希
+            int slotPos = keyHash % this.hashSlotNum; //根据哈希值取余哈希槽数量，得到索引
+            int absSlotPos = IndexHeader.INDEX_HEADER_SIZE + slotPos * hashSlotSize; //拿到该索引哈希槽的起始偏移
 
             try {
                 int slotValue = this.mappedByteBuffer.getInt(absSlotPos);
                 if (slotValue <= invalidIndex || slotValue > this.indexHeader.getIndexCount()
-                    || this.indexHeader.getIndexCount() <= 1) {
+                        || this.indexHeader.getIndexCount() <= 1) { //如果这个值无效(说明没有前置的index条目，即这个哈希槽只有这一个条目)，什么也不做
                 } else {
                     for (int nextIndexToRead = slotValue; ; ) {
                         if (phyOffsets.size() >= maxNum) {
                             break;
                         }
-
+                        //拿到映射到这个哈希槽的某个index条目的偏移
                         int absIndexPos =
                             IndexHeader.INDEX_HEADER_SIZE + this.hashSlotNum * hashSlotSize
                                 + nextIndexToRead * indexSize;
@@ -252,7 +281,7 @@ public class IndexFile {
                         boolean timeMatched = timeRead >= begin && timeRead <= end;
 
                         if (keyHash == keyHashRead && timeMatched) {
-                            phyOffsets.add(phyOffsetRead);
+                            phyOffsets.add(phyOffsetRead);  /**表示找到了一个 满足条件的消息，将它的偏移量放入到phyOffsets*/
                         }
 
                         if (prevIndexRead <= invalidIndex
